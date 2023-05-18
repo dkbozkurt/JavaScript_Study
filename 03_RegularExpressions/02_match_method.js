b983f17d5ddd56b38166b75abaeefaf20d79b435
dkbozkurt <dkaanbozkurt@gmail.com>
2023-05-19 00:47:05 +0200

    Match methods 3

--- 03_RegularExpressions/02_match_method.js
+++ 03_RegularExpressions/02_match_method.js
@@ -42,7 +42,7 @@ Note also work with numbers and Uppercase letters.
 
 let quoteSampleTwo = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/gi;
-let resultTwo = quoteSample.match(alphabetRegex); \
+let resultTwo = quoteSample.match(alphabetRegex);
 
 let quoteSampleThree = "Blueberry 3.141592653s are delicious.";
 let myRegex = /[h-s2-6]/ig;
@@ -121,3 +121,321 @@ any of the other quotes.
 let chewieRegex = /Aa*/; 
 let resultSix = chewieQuote.match(chewieRegex);
 
+// Find Characters with Lazy Matching
+/*
+You can apply the regex /t[a-z]*i/ to the string "titanic". This regex is 
+basically a pattern that starts with t, ends with i, and has some letters 
+in between.
+
+However, you can use the ? character to change it to lazy matching. "titanic" 
+matched against the adjusted regex of /t[a-z]*?i/ returns ["ti"].
+
+Note: Parsing HTML with regular expressions should be avoided, but pattern 
+matching an HTML string with regular expressions is completely fine.
+*/
+
+/*
+Q
+
+Fix the regex /<.*>/ to return the HTML tag <h1> and not the text "<h1>Winter 
+is coming</h1>". Remember the wildcard . in a regular expression matches any 
+character.
+*/
+
+let text = "<h1>Winter is coming</h1>";
+let myRegexSeven = /<.*?>/;
+let resultSeven = text.match(myRegex);
+
+
+/*
+Question :
+
+Time to pause and test your new regex writing skills. A group of criminals 
+escaped from jail and ran away, but you don't know how many. However, you do 
+know that they stay close together when they are around other people. You are
+responsible for finding all of the criminals at once.
+
+Here's an example to review how to do this:
+
+The regex /z+/ matches the letter z when it appears one or more times in a 
+row. It would find matches in all of the following strings:
+
+"z"
+"zzzzzz"
+"ABCzzzz"
+"zzzzABC"
+"abczzzzzzzzzzzzzzzzzzzzzabc"
+But it does not find matches in the following strings since there are no 
+letter z characters:
+
+""
+"ABC"
+"abcabc"
+Write a greedy regex that finds one or more criminals within a group of other 
+people. A criminal is represented by the capital letter C.
+
+*/
+
+let reCriminals = /C+/;
+
+// Match Beginning String Patterns
+/*
+In an earlier challenge, you used the caret character (^) inside a character 
+set to create a negated character set in the form [^thingsThatWillNotBeMatched]. 
+Outside of a character set, the caret is used to search for patterns at the 
+beginning of strings.
+
+let firstString = "Ricky is first and can be found.";
+let firstRegex = /^Ricky/;
+firstRegex.test(firstString);
+let notFirst = "You can't find Ricky now.";
+firstRegex.test(notFirst);
+The first test call would return true, while the second would return false.
+*/
+
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/;
+let resultEight = calRegex.test(rickyAndCal);
+
+// Match Ending String Patterns
+/*
+You can search the end of strings using the dollar sign character $ at the end 
+of the regex.
+
+let theEnding = "This is a never ending story";
+let storyRegex = /story$/;
+storyRegex.test(theEnding);
+let noEnding = "Sometimes a story will have to end";
+storyRegex.test(noEnding);
+
+The first test call would return true, while the second would return false.
+*/
+
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/;
+let resultNine = lastRegex.test(caboose);
+
+// Match All Letters and Numbers
+/*
+The closest character class in JavaScript to match the alphabet is \w. 
+This shortcut is equal to [A-Za-z0-9_]. This character class matches upper 
+and lowercase letters plus numbers. Note, this character class also includes 
+the underscore character (_).
+
+let longHand = /[A-Za-z0-9_]+/;
+let shortHand = /\w+/;
+let numbers = "42";
+let varNames = "important_var";
+longHand.test(numbers);
+shortHand.test(numbers);
+longHand.test(varNames);
+shortHand.test(varNames);
+
+All four of these test calls would return true.
+
+These shortcut character classes are also known as shorthand character classes.
+*/
+
+let quoteSampleTen = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/gi;
+let resultTen = quoteSampleTen.match(alphabetRegexV2).length;
+
+// Match Everything But Letters and Numbers
+/*
+You can search for the opposite of the \w with \W. Note, the opposite pattern 
+uses a capital letter. This shortcut is the same as [^A-Za-z0-9_].
+
+let shortHand = /\W/;
+let numbers = "42%";
+let sentence = "Coding!";
+numbers.match(shortHand);
+sentence.match(shortHand);
+
+The first match call would return the value ["%"] and the second would return ["!"].
+*/
+
+let quoteSampleEleven = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/gi;
+let resultEleven = quoteSampleEleven.match(nonAlphabetRegex).length;
+
+// Match All Numbers
+/*
+The shortcut to look for digit characters is \d, with a lowercase d. This is equal 
+to the character class [0-9], which looks for a single character of any number between 
+zero and nine.
+*/
+
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/gi;
+let resultTwelve = movieName.match(numRegex).length;
+
+// Match All Non-Numbers
+/*
+The shortcut to look for non-digit characters is \D. This is equal to the character 
+class [^0-9], which looks for a single character that is not a number between zero 
+and nine.
+*/
+
+let movieNameTwo = "2001: A Space Odyssey";
+let noNumRegex = /\D/g;
+let resultThirdteen = movieNameTwo.match(noNumRegex).length;
+
+// Restrict Possible Usernames
+/*
+QUESTION
+You need to check all the usernames in a database. Here are some simple rules that 
+users have to follow when creating their username.
+
+Usernames can only use alpha-numeric characters.
+
+The only numbers in the username have to be at the end. There can be zero or more 
+of them at the end. Username cannot start with the number.
+
+Username letters can be lowercase and uppercase.
+
+Usernames have to be at least two characters long. A two-character username can 
+only use alphabet letters as characters.
+
+Change the regex userCheck to fit the constraints listed above.
+*/
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; 
+// second solution : let userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i;
+let resultUserName = userCheck.test(username);
+
+// Match Whitespace
+/*
+You can search for whitespace using \s, which is a lowercase s. This pattern 
+not only matches whitespace, but also carriage return, tab, form feed, and new 
+line characters. You can think of it as similar to the character 
+class [ \r\t\f\n\v].
+
+let whiteSpace = "Whitespace. Whitespace everywhere!"
+let spaceRegex = /\s/g;
+whiteSpace.match(spaceRegex);
+
+This match call would return [" ", " "].
+*/
+/*
+Q : Change the regex countWhiteSpace to look for multiple whitespace 
+characters in a string. */
+
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g;
+let resultFourteen = sample.match(countWhiteSpace);
+
+// Match Non-Whitespace Characters
+/*
+Search for non-whitespace using \S, which is an uppercase s. This pattern 
+will not match whitespace, carriage return, tab, form feed, and new line 
+characters. You can think of it being similar to the character class 
+[^ \r\t\f\n\v].
+
+let whiteSpace = "Whitespace. Whitespace everywhere!"
+let nonSpaceRegex = /\S/g;
+whiteSpace.match(nonSpaceRegex).length;
+
+The value returned by the .length method would be 32.
+*/
+
+let sampleTwo = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g;
+let resultFifteen = sample.match(countNonWhiteSpace);
+
+// Specify Upper and Lower Number of Matches
+/*
+You can specify the lower and upper number of patterns with quantity specifiers. 
+Quantity specifiers are used with curly brackets ({ and }). You put two numbers 
+between the curly brackets - for the lower and upper number of patterns.
+
+For example, to match only the letter a appearing between 3 and 5 times in the 
+string ah, your regex would be /a{3,5}h/.
+
+let A4 = "aaaah";
+let A2 = "aah";
+let multipleA = /a{3,5}h/;
+multipleA.test(A4);
+multipleA.test(A2);
+The first test call would return true, while the second would return false.
+*/
+
+/* Q: Change the regex ohRegex to match the entire phrase Oh no only when it 
+has 3 to 6 letter h's. */
+
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6}\sno/;
+let resultSixteen = ohRegex.test(ohStr);
+
+// Specify Only the Lower Number of Matches
+/*
+To only specify the lower number of patterns, keep the first number followed 
+by a comma.
+
+For example, to match only the string hah with the letter a appearing at least 
+3 times, your regex would be /ha{3,}h/.
+
+let A4 = "haaaah";
+let A2 = "haah";
+let A100 = "h" + "a".repeat(100) + "h";
+let multipleA = /ha{3,}h/;
+multipleA.test(A4);
+multipleA.test(A2);
+multipleA.test(A100);
+
+In order, the three test calls would return true, false, and true.
+*/
+
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/;
+let resultSeventeen = haRegex.test(haStr);
+
+// Specify Exact Number of Matches
+/*
+To specify a certain number of patterns, just have that one number 
+between the curly brackets.
+
+For example, to match only the word hah with the letter a 3 times, 
+your regex would be /ha{3}h/.
+
+let A4 = "haaaah";
+let A3 = "haaah";
+let A100 = "h" + "a".repeat(100) + "h";
+let multipleHA = /ha{3}h/;
+multipleHA.test(A4);
+multipleHA.test(A3);
+multipleHA.test(A100);
+
+In order, the three test calls would return false, true, and false.
+*/
+
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/;
+let resultEighteen = timRegex.test(timStr);
+
+// Check for All or None
+/*
+You can specify the possible existence of an element with a question 
+mark, ?. This checks for zero or one of the preceding element. You can 
+think of this symbol as saying the previous element is optional.
+
+For example, there are slight differences in American and British English 
+and you can use the question mark to match both spellings.
+
+let american = "color";
+let british = "colour";
+let rainbowRegex= /colou?r/;
+rainbowRegex.test(american);
+rainbowRegex.test(british);
+
+Both uses of the test method would return true.
+*/
+
+let favWord = "favorite";
+let favRegex = /favou?rite/;
+let resultNineteen = favRegex.test(favWord);
+
+// Positive and Negative Lookahead
+/*
+
+*/
