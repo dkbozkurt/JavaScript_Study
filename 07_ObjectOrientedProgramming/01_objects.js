4c54183d743b5f1f8be41b0f1d51a1afa4784932
dkbozkurt <dkaanbozkurt@gmail.com>
2023-05-30 11:57:03 +0200

    OOP object 1

---
+++ 07_ObjectOrientedProgramming/01_objects.js
@@ -0,0 +1,24 @@
+// Create a Basic JavaScript Object
+/*
+Objects in JavaScript are used to model real-world objects, giving them
+properties and behavior just like their real-world counterparts. Here's an
+example using these concepts to create a duck object:
+
+let duck = { 
+    name: "Aflac", 
+    numLegs: 2
+};
+
+This duck object has two property/value pairs: a name of Aflac and a numLegs of
+2.
+
+*/
+
+let dog = {
+    name: "berkin",
+    numLegs: 4
+};
+
+// Use Dot Notation to Access the Properties of an Object
+
+console.log("Dog name: " + dog.name + "\nNumber of legs: " + dog.numLegs);
