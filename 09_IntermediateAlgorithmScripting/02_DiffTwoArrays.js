9865a5f9781d28355be483ae773b4ffc125f37ad
dkbozkurt <dkaanbozkurt@gmail.com>
2023-06-28 10:47:17 +0200

    Diff Two Arrays

---
+++ 09_IntermediateAlgorithmScripting/02_DiffTwoArrays.js
@@ -0,0 +1,19 @@
+// Diff Two Arrays
+/*
+Q 
+
+Compare two arrays and return a new array with any items only found in one of
+the two given arrays, but not both. In other words, return the symmetric
+difference of the two arrays.
+
+Note: You can return the array with its elements in any order.
+*/
+
+function diffArray(arr1, arr2) {
+    return arr1
+    .concat(arr2)
+    .filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+
+var res = diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(res);
