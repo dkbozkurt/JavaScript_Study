036f527c51261103306d40902101d561303a27f3
dkbozkurt <dkaanbozkurt@gmail.com>
2023-07-18 11:17:22 +0200

    Trivial

--- 09_IntermediateAlgorithmScripting/08_DNAPairing.js
+++ 09_IntermediateAlgorithmScripting/08_DNAPairing.js
@@ -45,21 +45,22 @@ function findMatch(char){
 pairElement("GCG");
 
 // Solutin 2
-
-// function pairElement(str) {
-//     // create object for pair lookup
-//     const pairs = {
-//       A: "T",
-//       T: "A",
-//       C: "G",
-//       G: "C"
-//     };
+/*
+function pairElement(str) {
+    // create object for pair lookup
+    const pairs = {
+      A: "T",
+      T: "A",
+      C: "G",
+      G: "C"
+    };
   
-//     // map character to array of character and matching pair
-//     return str
-//       .split("")
-//       .map(x => [x, pairs[x]]);
-//   }
+    // map character to array of character and matching pair
+    return str
+      .split("")
+      .map(x => [x, pairs[x]]);
+  }
   
-//   // test here
-//   pairElement("GCG");
+  // test here
+  pairElement("GCG");
+*/
