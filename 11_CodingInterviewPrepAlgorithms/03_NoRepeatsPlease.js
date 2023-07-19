0a8cf0839c1d2315de158428864ca51e4aafd872
dkbozkurt <dkaanbozkurt@gmail.com>
2023-07-19 14:22:24 +0200

    No Repeats Please

--- 11_CodingInterviewPrepAlgorithms/03_NoRepeatsPlease.js
+++ 11_CodingInterviewPrepAlgorithms/03_NoRepeatsPlease.js
@@ -10,7 +10,38 @@ aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter
 */
 
 function permAlone(str) {
-    return str;
-  }
-  
-  permAlone('aab');
+    let regex = /(.)\1+/;
+
+    const arr = str.split("");
+    const permutations = [];
+    let tmp;
+
+    if (str.match(regex) !== null && str.match(regex)[0] === str) return 0;
+    function swap(index1, index2) {
+        tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+
+    function generate(int) {
+        if (int === 1) {
+        permutations.push(arr.join(""));
+        } 
+        else {
+            for (let i = 0; i != int; ++i) {
+            generate(int - 1);
+            swap(int % 2 ? 0 : i, int - 1);
+            }
+        }
+    }
+
+    generate(arr.length);
+
+    const filtered = permutations.filter(function(string) {
+        return !string.match(regex);
+    });
+    return filtered.length;
+}
+
+var res = permAlone("aab");
+console.log(res);
