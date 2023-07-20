1f93cc68bc4ecdf2f427d55d5a2f3b473f248543
dkbozkurt <dkaanbozkurt@gmail.com>
2023-07-20 21:24:09 +0200

    Quick Sort

--- 11_CodingInterviewPrepAlgorithms/08_ImplementQuickSort.js
+++ 11_CodingInterviewPrepAlgorithms/08_ImplementQuickSort.js
@@ -20,7 +20,33 @@ for our purposes here. For simplicity, the first or last element could be used.
 */
 
 function quickSort(array) {
-    // Only change code below this line
-    return array;
-    // Only change code above this line
+
+    let size = array.length;
+
+    if (size <= 1) {
+        return array;
+    }
+
+    const pivot = array[Math.floor(size / 2)];
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < size ; i++) {
+        if (i === Math.floor(size / 2)) continue;
+
+        if (array[i] < pivot) {
+
+            left.push(array[i]);
+
+        } else {
+
+            right.push(array[i]);
+
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
+
+var res = quickSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]);
+console.log(res);
